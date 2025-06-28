d79e77c27686aaa7f02e8d384f53c7e9630c1aa0
fazlerabbi13 <mohammad0132794@gmail.com>
2025-06-28 16:15:19 +0600

    find the circle area

--- Inheritance.java
+++ Inheritance.java
@@ -1,28 +1,44 @@
 
-import java.awt.Rectangle;
-
-
 class Shape{
-    public int area(){
+    public double  area(){
         return 0;
     }
+}
 
     class Rectangle extends Shape{
-        public void area(int l,int b){
-            System.out.println(l*b);
+
+        private double l;
+        private double b;
+
+        public Rectangle(double l,double b){
+            this.l=l;
+            this.b=b;
+        }
+
+        // Overriding area() method for Rectangle
+        @Override
+        public double area(){
+            return l*b;
         }
     }
 
     class Circle extends Shape{
-        public void area(int r){
-            System.out.println(3.1416*r*r);
+        private double r;
+
+        public Circle(double r){
+            this.r=r;
+        }
+
+        @Override
+        public double area(){
+            return 3.1416*r*r;
         }
     }
-}
+
 
 public class Inheritance {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
-        rectangle.area();
+        Shape c = new Circle(5);
+        System.out.println("circle "+c.area());
     }
 }
