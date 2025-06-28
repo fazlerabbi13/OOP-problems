3ff0a333c4a5fc61f5e67c14323daa8bd0030f17
fazlerabbi13 <mohammad0132794@gmail.com>
2025-06-28 14:52:28 +0600

    Add a method displayDetails() to print employee info.In the main() method, create an object and use setters to assign values, then display details.

--- Encapsulation.java
+++ Encapsulation.java
@@ -25,12 +25,27 @@ class Employee{
     public void setName(String name){
         this.name=name;
     }
-    
+
     public void setSalary(int salary){
         this.salary=salary;
+    }
+
+
+    public void details(){
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(salary);
     }
 }
 
 public class Encapsulation {
-    
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+
+        employee.setId(123);
+        employee.setName("Rabbi");
+        employee.setSalary(500000000);
+
+        employee.details();
+    }
 }
